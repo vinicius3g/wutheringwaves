06a7462b7a588d2c06ce5f91d1119fd2ffb6f7e6
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-06-05 15:53:40 -0300

    feat(logo): adiçao de logo na pagina

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,16 @@
 import { BackgroundBox } from '@/components/Background';
 import PageContent from '@/components/PageContent';
 
-import styles from './Home.module.scss'
+import styles from './Home.module.scss';
+import TopBar from '@/components/TopBar';
 
 export default function Home() {
   return (
     // flex min-h-screen flex-col items-center justify-between p-24
     <main className={styles.pc_wrapper}>
       <BackgroundBox />
+      <div className={styles.logo} />
+      <TopBar />
       <PageContent />
     </main>
   );
